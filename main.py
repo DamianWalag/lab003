6d1e9a8f9a75b93066a67dafd2d010c8d30da8e2
damianhealthup <damian@healthup.pl>
2022-11-23 12:37:32 +0100

    final

--- main.py
+++ main.py
@@ -33,8 +33,8 @@ print("sredni czas: ",np.arange.srednia)
 print("max czas: ",np.arange.max)
 print("min czas: ",np.arange.min)
 print("odchylenie czasow: ",np.arange.std)
-for n in range(5):
-    start = time.time()
-    a = np.arange(1000000000)
-    stop = time.time()
-    print(stop-start)
+# for n in range(5):
+#     start = time.time()
+#     a = np.arange(1000000000)
+#     stop = time.time()
+#     print(stop-start)
